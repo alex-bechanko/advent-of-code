29047b7c1525fabb52d18b8f4eed4750d340b519
Alex Bechanko <alexbechanko@gmail.com>
2023-11-30 23:55:31 -0500

    Add license checker, format rust files with license checker

--- flake.nix
+++ flake.nix
@@ -3,25 +3,34 @@
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+
+    licensure-dev = {
+      url = "github:chasinglogic/licensure";
+      flake = false;
+    };
  
   };
 
   description = "Advent of Code solutions written in Rust";
 
-  outputs = { self, nixpkgs, flake-utils, naersk }:
+  outputs = { self, nixpkgs, flake-utils, naersk, licensure-dev }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        naersk' = pkgs.callPackage naersk { };
+        naersk' = pkgs.callPackage naersk {};
         rust-toolchain = pkgs.symlinkJoin {
           name = "rust-toolchain";
           paths = [ pkgs.rustc pkgs.cargo pkgs.cargo-watch pkgs.rust-analyzer  pkgs.rustPlatform.rustcSrc pkgs.clippy
           pkgs.rustfmt];
         };
-      in {
+      in rec {
         defaultPackage = naersk'.buildPackage ./.;
+        licensure = naersk'.buildPackage {
+          src = licensure-dev;
+          buildInputs = [ pkgs.openssl.dev pkgs.pkg-config ];
+        };
         devShell = with pkgs; mkShell {
-          buildInputs = [ rust-toolchain pkgs.openssl.dev pkgs.pkg-config ];
+          buildInputs = [ rust-toolchain pkgs.openssl.dev pkgs.pkg-config licensure ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
         };
   });
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 /*
-Advent of Code Solutions
-Copyright (C) 2023 Alex Bechanko
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+* Advent of Code Solutions Copyright (C) 2023 Alex Bechanko
+* <alexbechanko@gmail.com>
+*
+* This program is free software: you can redistribute it and/or modify it under
+* the terms of the GNU General Public License as published by the Free Software
+* Foundation, either version 3 of the License, or (at your option) any later
+* version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+* details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
 
 use clap::Parser;
 use std::path::PathBuf;
