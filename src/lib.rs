5b99fe4d7ec343415e8d42b0736f5c6c72cc50dd
Alex Bechanko <alexbechanko@gmail.com>
2023-12-02 17:39:33 -0500

    Move yXXXX modules into lib module
    To make things easier to read and traverse, the modules y2022 and y2023
    were merged into the lib module. Now there's one place to look for
    solutions. This will also help for a later refactor when default inputs
    and puzzle parts are defined in a day module.

--- src/lib.rs
+++ src/lib.rs
@@ -16,8 +16,30 @@
 * this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-mod y2022;
-mod y2023;
+mod y2022 {
+    pub mod day01;
+    use crate::Solution;
+
+    pub const DAYS: [(Solution, Solution); 1] = [(&day01::part_a, &day01::part_b)];
+
+    pub const DAY_INPUTS: [&str; 1] = [include_str!("../inputs/2022-12-01.txt")];
+}
+
+mod y2023 {
+    mod day01;
+    mod day02;
+    use crate::Solution;
+
+    pub const DAYS: [(Solution, Solution); 2] = [
+        (&day01::part_a, &day01::part_b),
+        (&day02::part_a, &day02::part_b),
+    ];
+
+    pub const DAY_INPUTS: [&str; 2] = [
+        include_str!("../inputs/2023-12-01.txt"),
+        include_str!("../inputs/2023-12-02.txt"),
+    ];
+}
 
 pub type Solution<'a> = &'a dyn Fn(&str) -> Result<String, String>;
 pub const YEARS: [&[(Solution, Solution)]; 2] = [&y2022::DAYS, &y2023::DAYS];
