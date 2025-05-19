662263d6c5bc38122b5e1fc0157bd595744f5e06
Alex Bechanko <alexbechanko@gmail.com>
2025-05-18 22:30:14 -0400

    feat: add benchmark for computing all solutions
    A primary goal of this library/program is to compute a year's
    solutions in under 1 second.
    Benchmarks for individual solutions helps find particularly slow
    solutions, but there was no benchmark to see how long it takes to
    compute all solutions in a year.

---
+++ benches/individual_solutions.rs
@@ -0,0 +1,193 @@
+/*
+Advent of Code solutions written in the Rust programming language
+Copyright (C) 2025 Alexander Bechanko
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+fn read_input_file(file: &str) -> String {
+    let err = format!("Expected input file {file}");
+    std::fs::read_to_string(file).expect(&err)
+}
+
+#[allow(clippy::too_many_lines)]
+pub fn aoc2024(c: &mut Criterion) {
+    use aoc::y2024::*;
+    c.bench_function("2024-12-01 Part 1", |b| {
+        b.iter(|| aoc::y2024::day01::part1(black_box(&read_input_file("./inputs/2024-12-01.txt"))));
+    });
+    c.bench_function("2024-12-01 Part 2", |b| {
+        b.iter(|| aoc::y2024::day01::part2(black_box(&read_input_file("./inputs/2024-12-01.txt"))));
+    });
+
+    c.bench_function("2024-12-02 Part 1", |b| {
+        b.iter(|| aoc::y2024::day02::part1(black_box(&read_input_file("./inputs/2024-12-02.txt"))));
+    });
+    c.bench_function("2024-12-02 Part 2", |b| {
+        b.iter(|| aoc::y2024::day02::part2(black_box(&read_input_file("./inputs/2024-12-02.txt"))));
+    });
+
+    c.bench_function("2024-12-03 Part 1", |b| {
+        b.iter(|| aoc::y2024::day03::part1(black_box(&read_input_file("./inputs/2024-12-03.txt"))));
+    });
+    c.bench_function("2024-12-03 Part 2", |b| {
+        b.iter(|| aoc::y2024::day03::part2(black_box(&read_input_file("./inputs/2024-12-03.txt"))));
+    });
+
+    c.bench_function("2024-12-04 Part 1", |b| {
+        b.iter(|| aoc::y2024::day04::part1(black_box(&read_input_file("./inputs/2024-12-04.txt"))));
+    });
+    c.bench_function("2024-12-04 Part 2", |b| {
+        b.iter(|| aoc::y2024::day04::part2(black_box(&read_input_file("./inputs/2024-12-04.txt"))));
+    });
+
+    c.bench_function("2024-12-05 Part 1", |b| {
+        b.iter(|| aoc::y2024::day05::part1(black_box(&read_input_file("./inputs/2024-12-05.txt"))));
+    });
+    c.bench_function("2024-12-05 Part 2", |b| {
+        b.iter(|| aoc::y2024::day05::part2(black_box(&read_input_file("./inputs/2024-12-05.txt"))));
+    });
+
+    c.bench_function("2024-12-06 Part 1", |b| {
+        b.iter(|| aoc::y2024::day06::part1(black_box(&read_input_file("./inputs/2024-12-06.txt"))));
+    });
+    c.bench_function("2024-12-06 Part 2", |b| {
+        b.iter(|| aoc::y2024::day06::part2(black_box(&read_input_file("./inputs/2024-12-06.txt"))));
+    });
+
+    c.bench_function("2024-12-07 Part 1", |b| {
+        b.iter(|| aoc::y2024::day07::part1(black_box(&read_input_file("./inputs/2024-12-07.txt"))));
+    });
+    c.bench_function("2024-12-07 Part 2", |b| {
+        b.iter(|| aoc::y2024::day07::part2(black_box(&read_input_file("./inputs/2024-12-07.txt"))));
+    });
+
+    c.bench_function("2024-12-08 Part 1", |b| {
+        b.iter(|| aoc::y2024::day08::part1(black_box(&read_input_file("./inputs/2024-12-08.txt"))));
+    });
+    c.bench_function("2024-12-08 Part 2", |b| {
+        b.iter(|| aoc::y2024::day08::part2(black_box(&read_input_file("./inputs/2024-12-08.txt"))));
+    });
+
+    c.bench_function("2024-12-09 Part 1", |b| {
+        b.iter(|| aoc::y2024::day09::part1(black_box(&read_input_file("./inputs/2024-12-09.txt"))));
+    });
+    c.bench_function("2024-12-09 Part 2", |b| {
+        b.iter(|| aoc::y2024::day09::part2(black_box(&read_input_file("./inputs/2024-12-09.txt"))));
+    });
+
+    c.bench_function("2024-12-10 Part 1", |b| {
+        b.iter(|| aoc::y2024::day10::part1(black_box(&read_input_file("./inputs/2024-12-10.txt"))));
+    });
+    c.bench_function("2024-12-10 Part 2", |b| {
+        b.iter(|| aoc::y2024::day10::part2(black_box(&read_input_file("./inputs/2024-12-10.txt"))));
+    });
+
+    c.bench_function("2024-12-11 Part 1", |b| {
+        b.iter(|| aoc::y2024::day11::part1(black_box(&read_input_file("./inputs/2024-12-11.txt"))));
+    });
+    c.bench_function("2024-12-11 Part 2", |b| {
+        b.iter(|| aoc::y2024::day11::part2(black_box(&read_input_file("./inputs/2024-12-11.txt"))));
+    });
+
+    c.bench_function("2024-12-12 Part 1", |b| {
+        b.iter(|| aoc::y2024::day12::part1(black_box(&read_input_file("./inputs/2024-12-12.txt"))));
+    });
+    c.bench_function("2024-12-12 Part 2", |b| {
+        b.iter(|| aoc::y2024::day12::part2(black_box(&read_input_file("./inputs/2024-12-12.txt"))));
+    });
+
+    c.bench_function("2024-12-13 Part 1", |b| {
+        b.iter(|| aoc::y2024::day13::part1(black_box(&read_input_file("./inputs/2024-12-13.txt"))));
+    });
+    c.bench_function("2024-12-13 Part 2", |b| {
+        b.iter(|| aoc::y2024::day13::part2(black_box(&read_input_file("./inputs/2024-12-13.txt"))));
+    });
+
+    c.bench_function("2024-12-14 Part 1", |b| {
+        b.iter(|| aoc::y2024::day14::part1(black_box(&read_input_file("./inputs/2024-12-14.txt"))));
+    });
+    c.bench_function("2024-12-14 Part 2", |b| {
+        b.iter(|| aoc::y2024::day14::part2(black_box(&read_input_file("./inputs/2024-12-14.txt"))));
+    });
+
+    c.bench_function("2024-12-15 Part 1", |b| {
+        b.iter(|| aoc::y2024::day15::part1(black_box(&read_input_file("./inputs/2024-12-15.txt"))));
+    });
+    c.bench_function("2024-12-15 Part 2", |b| {
+        b.iter(|| aoc::y2024::day15::part2(black_box(&read_input_file("./inputs/2024-12-15.txt"))));
+    });
+
+    c.bench_function("2024-12-16 Part 1", |b| {
+        b.iter(|| aoc::y2024::day16::part1(black_box(&read_input_file("./inputs/2024-12-16.txt"))));
+    });
+    c.bench_function("2024-12-16 Part 2", |b| {
+        b.iter(|| aoc::y2024::day16::part2(black_box(&read_input_file("./inputs/2024-12-16.txt"))));
+    });
+
+    c.bench_function("2024-12-17 Part 1", |b| {
+        b.iter(|| aoc::y2024::day17::part1(black_box(&read_input_file("./inputs/2024-12-17.txt"))));
+    });
+    c.bench_function("2024-12-17 Part 2", |b| {
+        b.iter(|| aoc::y2024::day17::part2(black_box(&read_input_file("./inputs/2024-12-17.txt"))));
+    });
+
+    c.bench_function("2024-12-18 Part 1", |b| {
+        b.iter(|| aoc::y2024::day18::part1(black_box(&read_input_file("./inputs/2024-12-18.txt"))));
+    });
+    c.bench_function("2024-12-18 Part 2", |b| {
+        b.iter(|| aoc::y2024::day18::part2(black_box(&read_input_file("./inputs/2024-12-18.txt"))));
+    });
+
+    c.bench_function("2024-12-19 Part 1", |b| {
+        b.iter(|| aoc::y2024::day19::part1(black_box(&read_input_file("./inputs/2024-12-19.txt"))));
+    });
+    c.bench_function("2024-12-19 Part 2", |b| {
+        b.iter(|| aoc::y2024::day19::part2(black_box(&read_input_file("./inputs/2024-12-19.txt"))));
+    });
+
+    c.bench_function("2024-12-20 Part 1", |b| {
+        b.iter(|| aoc::y2024::day20::part1(black_box(&read_input_file("./inputs/2024-12-20.txt"))));
+    });
+    c.bench_function("2024-12-20 Part 2", |b| {
+        b.iter(|| aoc::y2024::day20::part2(black_box(&read_input_file("./inputs/2024-12-20.txt"))));
+    });
+
+    c.bench_function("2024-12-21 Part 1", |b| {
+        b.iter(|| aoc::y2024::day21::part1(black_box(&read_input_file("./inputs/2024-12-21.txt"))));
+    });
+    c.bench_function("2024-12-21 Part 2", |b| {
+        b.iter(|| aoc::y2024::day21::part2(black_box(&read_input_file("./inputs/2024-12-21.txt"))));
+    });
+
+    c.bench_function("2024-12-22 Part 1", |b| {
+        b.iter(|| aoc::y2024::day22::part1(black_box(&read_input_file("./inputs/2024-12-22.txt"))));
+    });
+    c.bench_function("2024-12-22 Part 2", |b| {
+        b.iter(|| aoc::y2024::day22::part2(black_box(&read_input_file("./inputs/2024-12-22.txt"))));
+    });
+
+    c.bench_function("2024-12-23 Part 1", |b| {
+        b.iter(|| aoc::y2024::day23::part1(black_box(&read_input_file("./inputs/2024-12-23.txt"))));
+    });
+    c.bench_function("2024-12-23 Part 2", |b| {
+        b.iter(|| aoc::y2024::day23::part2(black_box(&read_input_file("./inputs/2024-12-23.txt"))));
+    });
+}
+
+criterion_group!(solutions, aoc2024);
+
+criterion_main!(solutions);
